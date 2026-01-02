cc90d4244cfbe4b107b11d98386c9f0d8c782428
O.V. Rud <oleg.rud@natur.cuni.cz>
2026-01-02 13:49:35 +0100

    launch_kiosk.sh

---
+++ launch_kiosk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Tell the script which screen to use
+export DISPLAY=:0
+
+# 1. Kill any existing Chromium windows to avoid "double windows"
+pkill -f chromium
+
+# 2. Wait a moment to ensure it's closed
+sleep 10
+
+# 3. Launch Chromium with your specific settings
+chromium --noerrdialogs --disable-infobars --incognito --start-fullscreen --app=https://physchem.cz/youtube_videos/ &
+
+# Open the crontab editor:
+# crontab -e
+# Scroll to the very bottom and add this line:
+# 0 0 * * * /home/kvint/launch_kiosk.sh
