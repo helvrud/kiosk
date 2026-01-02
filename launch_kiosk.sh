66b1c2a71cade855b308e93eab8d85b823c5a7bc
O.V. Rud <oleg.rud@natur.cuni.cz>
2026-01-02 14:03:38 +0100

    launch_kiosk.sh

--- launch_kiosk.sh
+++ launch_kiosk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Tell the script which screen to use
 export DISPLAY=:0
+export XAUTHORITY=/home/kvint/.Xauthority
 
 # 1. Kill any existing Chromium windows to avoid "double windows"
 pkill -f chromium
@@ -9,7 +10,11 @@ pkill -f chromium
 sleep 10
 
 # 3. Launch Chromium with your specific settings
-chromium --noerrdialogs --disable-infobars --incognito --start-fullscreen --app=https://physchem.cz/youtube_videos/ &
+chromium --noerrdialogs --disable-infobars --incognito --start-fullscreen \
+    --app=https://physchem.cz/youtube_videos/ \
+    --ignore-gpu-blocklist \
+    --enable-gpu-rasterization \
+    --enable-zero-copy &
 
 # Open the crontab editor:
 # crontab -e
